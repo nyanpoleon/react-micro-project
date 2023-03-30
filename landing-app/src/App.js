ccd7f571bb1744bb5f0e8ae74ce177c73ca3fbcc
Purujit Pradhan <purujit26@gmail.com>
2023-03-30 10:42:28 +0530

    added menu prop

--- landing-app/src/App.js
+++ landing-app/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Heading from "./Heading";
+import Menu from "./Menu";
 import "./App.css";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <div className="header">
         <div className="left_header">
+
           <Heading
             heading="An inspiring design delivered to your inbox every morning"
             decription="Our team scouts the internet for the best designs, illustrations and art and delivers a truly inspiring one every day to your inbox"
@@ -24,7 +26,20 @@ function App() {
           <img src={require("./dweep.png")}></img>
         </div>
       </div>
-      <h1>Hello World</h1>
+      <div className="footer">
+        <div className="left_footer">
+          <Menu item="Prompt Generator"/>
+          <Menu item="Dweep Daily"/>
+          <Menu item="All Contributors"/>
+          <Menu item="Your data on Dweep.io"/>
+          <Menu item="Contribute to Dweep"/>
+          
+        </div>
+        <div className="right_footer">
+          Dweep.io
+          Made with love in India
+        </div>
+      </div>
     </div>
   );
 }
